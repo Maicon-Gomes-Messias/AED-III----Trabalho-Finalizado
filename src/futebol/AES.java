68bb92a9a89b08ec7e5f84bfb316fc90b69cf578
maicon gomes <maicon.gomesmessias@gmail.com>
2022-09-12 11:11:38 -0300

    AED III

---
+++ src/futebol/AES.java
@@ -0,0 +1,50 @@
+package futebol;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import java.util.Base64;
+
+
+//classe AES para criptografar strings
+
+public class AES {
+    private SecretKey key;
+    private final int KEY_SIZE = 128;
+    private final int T_LEN = 128;
+    private Cipher encryptionCipher;
+
+    public void init() throws Exception {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(KEY_SIZE);
+        key = generator.generateKey();
+    }
+
+    //metodo pra criptografar a string ela recebe a palavra e retorna ela criptografada
+    public String encrypt(String message) throws Exception {
+        
+        byte[] messageInBytes = message.getBytes();
+        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = encryptionCipher.doFinal(messageInBytes);
+        return encode(encryptedBytes);
+    }
+    //metodo para descriptografar recece a palavra criptografada acima e realiza o processo de descriptografia
+    public String decrypt(String mensagem_criptografada) throws Exception {
+        byte[] messageInBytes = decode(mensagem_criptografada);
+        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(T_LEN, encryptionCipher.getIV());
+        decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
+        byte[] decryptedBytes = decryptionCipher.doFinal(messageInBytes);
+        return new String(decryptedBytes);
+    }
+
+    private String encode(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    private byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+}
